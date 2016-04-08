9d10e52575a7fa7dcf97efb63d81570109e3fd1a
August Wande <wandeaugust@gmail.com>
2016-04-08 18:19:35 +0200

    rwd och cart typ klar.

--- js/js.js
+++ js/js.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$('.popupShadow').hide();
 	$('.popupImage').hide();
-	imagePopup('.product-image');
+	
+	imagePopup('.product-image, .product-image-enlarge');
 	listener();
 	var popupFunction; //håller vilken popuptyp som öppnades senast
+
 	function imagePopup(imageClass){
 		$(document).on('click', imageClass, function() {
 			popupImage('<img src="' + $(imageClass).attr('src') + '" alt="Image not available">');
@@ -15,12 +17,13 @@ $(document).ready(function() {
 		$('.popupImage').empty().append(content);
 		popupFunction = 'popupImage';
 		fadeIn('.popupImage', 500);
+		fadeIn('.popupShadow', 500);
 		
 	}
 
 	function fadeIn(element, ms) {
 		$(element).hide().fadeIn(ms);
-		$('.popupShadow').fadeIn(ms);
+		
 
 	}
 
@@ -29,8 +32,44 @@ $(document).ready(function() {
 		$('.popupShadow').fadeOut(ms);
 	}
 
-	function listener() { //lyssnar efter saker
+	function showMenuSmall() {
+		$('.menu-small').animate({
+			top:0
+		}, 200, function() {
+			//klar
+		});
+		fadeOut('.menu-burger', 50);
+	}
+	function hideMenuSmall() {
+		$('.menu-small').animate({
+			top:'-600px'
+		}, 200, function() {
+			//klar
+		});
+		fadeIn('.menu-burger', 50);
+	}
 
+
+	function listener() { //lyssnar efter saker
+		//menu-small
+		$('.menu-burger').click(function() {  //show menu
+			showMenuSmall();
+		});
+
+		$('.menu-small-hide').click(function() {
+			hideMenuSmall();
+		});
+
+		$(document).on('click', '.menu-link', function() {
+			hideMenuSmall();
+		});
+
+		$(document).on('click', '.shopping-cart > a', function() {
+			alert("HEJ");
+		});
+
+
+		///////// Popup
 		function closePopup() { //stänger den popuptyp som har öppnats
 			switch (popupFunction) {
 				case 'popupImage':
--- js/script.js
+++ js/script.js
@@ -24,12 +24,54 @@ var app = angular.module('webshopApp', ['ngRoute', 'ngMaterial']);
 
             .when('/product-page', {
             	templateUrl : './pages/product-page.html',
-            	controller : 'pagesController'
-            });
+            	controller : 'productPageController'
+            })
+
+            .when('/shopping-cart', {
+            	templateUrl : './pages/shopping-cart.html',
+            	controller : 'cartController'
+            });//semikolon för sista
     });
 
     app.controller('mainController', function($scope) {
     	//product-item hoover "show more"
+    	$scope.arrCartItems = []; //array som håller alla items som lagts till i shopping-cart
+    	$scope.totalPrice = 0;
+
+    	//productItem.id, productItem.name, productItem.imageurl, productPrice.value, quantity
+    	$scope.addToCart = function($id, $name, $imageurl, $price, $quantity) {
+    		var matchFound = false;
+    		
+    		
+	    		for (var i = 0; i < $scope.arrCartItems.length; i++) {
+	    			if ($id == $scope.arrCartItems[i].id) { //om item redan finns i shopping-cart
+	    				matchFound = true; //matching hittades i arrayen
+	    				cartGetTotal(); //när antal läggs till så måste totalpriset uppdateras
+	    				$scope.arrCartItems[i].quantity += $quantity; //om den redan finns, lägg till på antal
+	    				break; //avsluta loopen
+	    			}
+	    		}
+
+    		if (matchFound == false) { //om matchning hittades i loopen
+    			addCartItem(); //lägg till item i shopping-cart
+    			cartGetTotal(); //räkna med item i totalsumman
+    		}
+    		
+    		function addCartItem() {
+    		$scope.arrCartItems.push({
+		    			"id" : $id,
+		    			"name" : $name,
+		    			"imageurl" : $imageurl,
+		    			"price" : $price,
+		    			"quantity" : $quantity
+		    });
+    		}
+    		function cartGetTotal() {
+    			$scope.totalPrice += $price * $quantity;
+    		}
+
+    	
+    	} //end addtocart
 
     	$scope.showProductPage = function($productItem) {
         		$scope.productItem = $productItem;
@@ -42,9 +84,9 @@ var app = angular.module('webshopApp', ['ngRoute', 'ngMaterial']);
     			
     			$http({
     			method:'GET',
-    			url:'http://83.255.163.37/webshopbackend/product/getpopular'})
+    			url:'http://83.255.163.37/webshopbackend/product/getallsale'})
     			.then(function(response) {
-    				$scope.productAllPopular = response.data;
+    				$scope.productAllSale = response.data;
     			});
     	
     }); //end startController
@@ -65,13 +107,30 @@ var app = angular.module('webshopApp', ['ngRoute', 'ngMaterial']);
         $scope.message = 'Contact us! JK. This is just a demo, about.';
     }); // end aboutController
 
-    app.controller('pagesController', function($scope) { //hantering av produktsidan
-        $scope.amount = 1; //antal varor
-    	
-    	$scope.$watch('amount', function(){ //antal varor kan aldrig vara mindre än 0
-    		if ($scope.amount < 0) {
-    			$scope.amount = 0;
+    app.controller('productPageController', function($scope) { //hantering av produktsidan
+        $scope.quantity = 1; //antal varor
+    	if ($scope.productItem.sale > 0) {
+    		$scope.productPrice = {sale:true,value:$scope.productItem.price - $scope.productItem.price * $scope.productItem.sale};
+
+    	} else {
+    		$scope.productPrice = {sale:false, value: $scope.productItem.price};
+    	}
+    	$scope.$watch('quantity', function(){ //antal varor kan aldrig vara mindre än 0
+    		if ($scope.quantity < 0) {
+    			$scope.quantity = 0;
     		}
     	});
 
     }); // end aboutController
+
+    //shopping-cart
+    app.controller('cartController', function($scope) {
+    	//code
+
+    });//end cartcontroller
+
+    app.filter('noDecimal', function() { //avrundar 
+    	return function (input) {
+    		return Math.round(input);
+    	};
+    });
